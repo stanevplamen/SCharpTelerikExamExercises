194f269636672c3d6f72ab62076bd9a1d7313ec3
stanevplamen <stanev.plamen@gmail.com>
2013-08-06 17:03:23 +0300

    added two tasks from DSA sample exam

---
+++ AcademyTask/Task.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Task
+{
+    static int[,] matrixSums;
+    static int[] numbersArr;
+    static int rowIndex;
+    static int ColIndex;
+    static List<Tuple<int, int>> impIndexes;
+
+    static void Main()
+    {
+        string allNumbers = Console.ReadLine();
+        string[] splitedInputNumbs = allNumbers.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+        int numberToCompare = int.Parse(Console.ReadLine());
+
+        LoadTheNumbers(splitedInputNumbs);
+
+        LoadTheSums();
+
+        FindTheResultIndexes(numberToCompare);
+
+        CalculateAndPrintResult();
+    }
+
+    private static void CalculateAndPrintResult()
+    {
+        int tempResult = 0;
+        int minResult = int.MaxValue;
+
+        foreach (var pair in impIndexes)
+        {
+            int rowSign = pair.Item1 % 2;
+            int colSign = pair.Item2 % 2;
+
+            int rowDiv = pair.Item1 / 2;
+            int colDiv = pair.Item2 / 2;
+
+            if (rowSign == 1 && colSign == 1)
+            {
+                tempResult = Math.Max(rowDiv, colDiv) + 1;
+            }
+            else if (rowSign == 0 && colSign == 0)
+            {
+                tempResult = Math.Max(rowDiv, colDiv) + 1;
+            }
+            else if (rowSign == 1 && colSign == 0)
+            {
+                tempResult = Math.Max(rowDiv, colDiv) + 1;
+            }
+            else if (rowSign == 0 && colSign == 1 && pair.Item1 > pair.Item2)
+            {
+                tempResult = Math.Max(rowDiv, colDiv) + 1;
+            }
+            else if (rowSign == 0 && colSign == 1 && pair.Item1 < pair.Item2)
+            {
+                tempResult = Math.Max(rowDiv, colDiv) + 2;
+            }
+            if (tempResult < minResult)
+            {
+                minResult = tempResult;
+            }
+        }
+        if (minResult < int.MaxValue)
+        {
+            Console.WriteLine(minResult);
+        }
+        else
+        {
+            Console.WriteLine(numbersArr.Length);
+        }
+    }
+
+    private static void FindTheResultIndexes(int numberToCompare)
+    {
+        impIndexes = new List<Tuple<int, int>>();
+        for (int row = 1; row < matrixSums.GetLength(0); row++)
+        {
+            for (int col = 1; col < matrixSums.GetLength(1); col++)
+            {
+                if (matrixSums[row, col] >= numberToCompare)
+                {
+                    rowIndex = row;
+                    ColIndex = col;
+                    impIndexes.Add(new Tuple<int, int>(rowIndex, ColIndex));
+                }
+            }
+        }
+    }
+
+    private static void LoadTheSums()
+    {
+        matrixSums = new int[numbersArr.Length + 1, numbersArr.Length + 1];
+
+        for (int row = 1; row < matrixSums.GetLength(0); row++)
+        {
+            for (int col = 1; col < matrixSums.GetLength(1); col++)
+            {
+                matrixSums[row, col] = (numbersArr[row - 1] - numbersArr[col - 1]);
+            }
+        }
+    }
+
+    private static void LoadTheNumbers(string[] splitedInputNumbs)
+    {
+        numbersArr = new int[splitedInputNumbs.Length];
+
+        for (int i = 0; i < numbersArr.Length; i++)
+        {
+            numbersArr[i] = int.Parse(splitedInputNumbs[i].Trim());
+        }
+    }
+}
---
+++ MessageToDecode/Message.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Message
+{
+    static List<string> numbsList;
+    static List<char> charList;
+    static List<string> finalOutput;
+
+    static void Main()
+    {
+        string inputNumbersString = Console.ReadLine();
+        string textToDecode = Console.ReadLine();
+
+        SplitTheINput(textToDecode);
+
+        finalOutput = new List<string>();
+        FindCodeMatches(inputNumbersString, String.Empty);
+
+        Print();    
+    }
+
+    private static void FindCodeMatches(string cipher, string current)
+    {
+        if (cipher.Length == 0)
+        {
+            finalOutput.Add(current);
+            return;
+        }
+        for (int i = 0; i < numbsList.Count; i++)
+        {
+            if (cipher.StartsWith(numbsList[i]))
+            {
+                FindCodeMatches(cipher.Substring(numbsList[i].Length), current + charList[i]);
+            }
+        }
+    }
+
+    private static void SplitTheINput(string text)
+    {
+        charList = new List<char>();
+        numbsList = new List<string>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] >= 65 && text[i] <= 90 && i == 0)
+            {
+                charList.Add(text[i]);
+            }
+            else if (text[i] >= 65 && text[i] <= 90)
+            {
+                charList.Add(text[i]);
+                numbsList.Add(sb.ToString());
+                sb.Clear();
+            }
+            else
+            {
+                sb.Append(text[i]);
+            }
+        }
+        numbsList.Add(sb.ToString());
+        sb.Clear();
+    }
+
+    private static void Print()
+    {
+        finalOutput.Sort();
+        Console.WriteLine(finalOutput.Count);
+        foreach (var decode in finalOutput)
+        {
+            Console.WriteLine(decode);
+        }
+    }
+}
+
+
